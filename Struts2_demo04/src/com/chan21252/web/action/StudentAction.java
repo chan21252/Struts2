a3a0cbdc27d6f6a39729c3b79154dd3582b02c7e
chan21252 <chan21252@gmail.com>
2019-04-11 23:04:48 +0800

    result元素使用

---
+++ Struts2_demo04/src/com/chan21252/web/action/StudentAction.java
@@ -0,0 +1,32 @@
+package com.chan21252.web.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class StudentAction extends ActionSupport {
+
+    private final String ADD_STUDENT = "addStudent";
+    private final String DELETE_STUDENT = "deleteStudent";
+    private final String UPDATE_STUDENT = "updateStudent";
+    private final String QUERY_STUDENT = "queryStudent";
+
+    public String addStudent(){
+        System.out.println("add student");
+        return SUCCESS;
+    }
+
+
+    public String deleteStudent(){
+        System.out.println("delete student");
+        return SUCCESS;
+    }
+
+    public String updateStudent(){
+        System.out.println("update student");
+        return SUCCESS;
+    }
+
+    public String queryStudent(){
+        System.out.println("query student");
+        return SUCCESS;
+    }
+}
