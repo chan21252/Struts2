6d81cc8584ad9dd653d82eeea039cfc78c367411
chan21252 <chanjobs21252@gamil.com>
2019-04-14 22:57:10 +0800

    动作转发/重定向到另一个动作

--- Struts2_demo04/src/com/chan21252/web/action/StudentAction.java
+++ Struts2_demo04/src/com/chan21252/web/action/StudentAction.java
@@ -9,24 +9,29 @@ public class StudentAction extends ActionSupport {
     private final String UPDATE_STUDENT = "updateStudent";
     private final String QUERY_STUDENT = "queryStudent";
 
-    public String addStudent(){
+    public String addStudent() {
         System.out.println("add student");
         return SUCCESS;
     }
 
 
-    public String deleteStudent(){
+    public String deleteStudent() {
         System.out.println("delete student");
         return SUCCESS;
     }
 
-    public String updateStudent(){
+    public String updateStudent() {
         System.out.println("update student");
         return SUCCESS;
     }
 
-    public String queryStudent(){
+    public String queryStudent() {
         System.out.println("query student");
+        return SUCCESS;
+    }
+
+    public String successAction() {
+        System.out.println("success action");
         return SUCCESS;
     }
 }
